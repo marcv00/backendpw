f2c9fb3603f2627e5d717f848ba4acd24677d974
Marco Villanueva <villadev25@gmail.com>
2025-07-14 08:06:48 -0500

    eojfnon

--- src/controllers/PlataformasController.ts
+++ src/controllers/PlataformasController.ts
@@ -1,10 +1,20 @@
 import express, { Request, Response } from "express"
-// import { PrismaClient, Plataforma } from "../generated/prisma"
+import { PrismaClient, Plataforma } from "../generated/prisma"
 
 const PlataformasController = () => {
     const router = express.Router()
 
-    
+    // GET /
+    router.get("/", async (_req: Request, res: Response) => {
+        const prisma = new PrismaClient()
+        try {
+            const plataformas = await prisma.plataforma.findMany()
+            res.json(plataformas)
+        } catch (error) {
+            console.error("Error al obtener plataformas:", error)
+            res.status(500).json({ error: "Error interno al obtener las plataformas" })
+        }
+    })
 
     return router
 }
